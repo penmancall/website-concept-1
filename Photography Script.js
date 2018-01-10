dc11a1e9366ef409c6adcac96d2597549d04f18a
penmancall <33585183+penmancall@users.noreply.github.com>
2018-01-10 14:35:22 +1300

    Add files via upload

---
+++ Photography Script.js
@@ -0,0 +1,10 @@
+//thanks to fancybox for open/close effect on photo gallery
+
+$(document).ready(function(){
+    $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+});
+   
+  
